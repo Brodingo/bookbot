68b90cfbec0e3b96342410510169510bfad6e73a
Brodingo <brodyk@gmail.com>
2024-09-23 21:16:12 -0400

    format char list with sorting

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = get_num_chars(text)
+    char_list = char_dict_list(char_count)
+    char_list.sort(reverse=True,key=sort_on)
+
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(char_count)
+    for char in char_list:
+        print(f"The '{char['name']}' character was found {char['num']} times")
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -24,5 +30,15 @@ def get_num_chars(text):
         else:
             chars[char] = 1
     return chars
+
+def char_dict_list(dict):
+    dict_list = []
+    for char in dict:
+        if char.isalpha():
+            dict_list.append({"name":char,"num":dict[char]})
+    return dict_list
+
+def sort_on(dict):
+    return dict["num"]
 
 main()
